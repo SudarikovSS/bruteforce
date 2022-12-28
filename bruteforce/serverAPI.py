4ac120b3310d495a2fbd94d6472a24c2ac9d2330
Stanislav <99170730+SudarikovSS@users.noreply.github.com>
2022-12-28 16:12:54 +0400

    Add files via upload

---
+++ bruteforce/serverAPI.py
@@ -0,0 +1,13 @@
+import aiohttp
+
+async def login(client: aiohttp.ClientSession, username: str, password: str):
+    json = {
+        "username": username,
+        "password": password
+    }
+    async with client.post('http://127.0.0.1:8888/api/login/', json=json) as resp:
+        json_data = await resp.json()
+        if json_data.get("status") == 'ok':
+            return True
+        else:
+            return False
